9eabdcb5f2f18ffd690125043d99e1e5fe626f54
Emmanuel <girault.e@gmail.com>
2022-08-20 23:57:45 +0200

    Added latest scripts regarding the day's lessons

--- example.js
+++ example.js
@@ -3,4 +3,27 @@ let thisArray=[2,4,6,8,10,12,14];
 function mult(a) {
     return a.reduce((start,next)=>start*next,1);
 }
-console.log(mult(thisArray));
+// console.log(mult(thisArray));
+
+function foobar(n) {
+    let result={};
+    for(let i=1;i<=n; i++) {
+        if(i%3===0 && i%5===0)result[i]="foobar";
+        else if(i%3===0)result[i]="foo";
+        else if(i%5===0)result[i]="bar";
+        else result[i]=i;
+    }
+    return result;
+}
+
+// console.log(foobar(8));
+
+function letterinastring(s) {
+    let result=[];
+    let len=s.length
+    for(let i=0;i<len;i++) {
+        if(i%3==0)result.push(s[i]);
+    }
+    return result.reverse().join("");
+}   
+console.log(letterinastring("this is a beautifulstring"));
