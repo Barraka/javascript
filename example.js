fea790e8c0808ddd3b73bb7c7627a8b7d2312a69
Emmanuel <girault.e@gmail.com>
2022-10-15 23:55:46 +0200

    Adding scripts regarding to async

--- example.js
+++ example.js
@@ -1,25 +1,37 @@
-let myimg = document.querySelector('.mypic');
-let output;
-let fetch = require('node-fetch');
-fetch('https://www.bbc.com', {
-    mode:'cors',
-    method:'GET',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    referrerPolicy: 'no-referrer',
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+const getReddit = async () => {
+  // get html text from reddit
+//   const response = await fetch('https://reddit.com/', {
+  const response = await fetch('./example.txt', {
+    method: 'GET',
     headers: {
-        'Content-Type': 'application/json'
-    },
+        "Content-Type": "application/text"
+      },
+    mode:'cors',
+    // credentials: 'same-origin', 
+    credentials: 'include',
+    redirect: 'follow',
+    referrerPolicy: 'same-origin',
     
-})
-.then(x=>{
-    console.log(x);
-    return x.json();
-})
-.then(x=>{
-    output=x;
-    console.log(x);
-})
-.catch(e=>{
-    console.log(e)
 });
+  // using await to ensure that the promise resolves
+  const body = await response.text();
+
+  // parse the html text and extract titles
+  const $ = cheerio.load(body);
+  const titleList = [];
+    
+  // using CSS selector  
+  $('._eYtD2XCVieq6emjKBH3m').each((i, title) => {
+    const titleNode = $(title);
+    const titleText = titleNode.text();
+    
+    titleList.push(titleText);
+  });
+
+  console.log(titleList);
+};
+
+getReddit();
